75e2d61bf82092d877442cd247a5f29ddf37697d
KHALEDEITTA <khal3dsa3d3@gmail.com>
2023-12-04 22:13:53 +0200

    setup main.yml

--- abdo.js
+++ abdo.js
@@ -1,2 +1,13 @@
-const myname = "خالد سعيد عبدالمقصود ابراهيم عيطه";
-console.log("My Name Is :" + myname);
+const express = require('express');
+const app = express();
+const port = 3000; // You can choose a different port
+
+// Define a route to print "Abdelhamid Salah"
+app.get('/printName', (req, res) => {
+  res.send('khaled said eita');
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
